639366135b178283aab2855c5cc4fef84899a318
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-04-17 13:59:51 -0300

    feat: navigation script

---
+++ src/public/js/index.js
@@ -0,0 +1,6 @@
+document.getElementById('log-button')
+    .addEventListener("click", (e) => {
+        //i know that i can use the <a> tag to make this navigation
+        //but,i use this script to serve at least 1 javascript 
+        window.location.href = "/login";
+    })
