d29aabb910f7ae75574a655e39ea1a109219876a
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-04-18 21:28:56 -0300

    chore: sign_up route registred in server

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ import (
 func dashboard(c echo.Context) error {
 	return c.Render(http.StatusOK, "index", nil)
 }
+func sign_up(c echo.Context) error {
+	return c.Render(http.StatusOK, "sign-up", nil)
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/", dashboard)
+	e.GET("/sign-up", sign_up)
 	//here we setup the static files system
 	e.Static("/public", "src/public")
 	e.Renderer = &Template{
